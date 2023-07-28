d1db032925cf37e90e31c285d2648d56eafdbb18
spandanakoppaku <skoppaku@gmail.com>
2023-07-27 20:42:15 -0400

    Auto Push

--- route-table.tf
+++ route-table.tf
@@ -13,6 +13,14 @@ resource "aws_route_table" "public_rt" {
   }
 }
 
+# Associates the route table to public subnet
+
+resource "aws_route_table_association" "public_subnet_rt_association" {
+  count          = lenght(aws_subnet.public_subnet.*.id)
+  subnet_id      = aws_subnet.foo.id
+  route_table_id = aws_route_table.bar.id
+}
+
 # Create Private Route Table
 resource "aws_route_table" "private_rt" {
   vpc_id            = aws_vpc.main.id
