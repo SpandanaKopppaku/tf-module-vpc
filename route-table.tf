6cd0777513c25ef87b5218ae049ff2c602dc84e2
spandanakoppaku <skoppaku@gmail.com>
2023-07-28 12:20:47 -0400

    Auto Push

--- route-table.tf
+++ route-table.tf
@@ -16,7 +16,7 @@ resource "aws_route_table" "public_rt" {
 # Associates the route table to public subnet
 
 resource "aws_route_table_association" "public_subnet_rt_association" {
-  count          = lenght(aws_subnet.public_subnet.*.id)
+  count          = length(aws_subnet.public_subnet.*.id)
   subnet_id      = element(aws_subnet.public_subnet.*.id, count.index)
   route_table_id = aws_route_table.public_rt.id
 }
