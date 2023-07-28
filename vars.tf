243e02bc10f2dcf356c28e817c3c4f714551cd64
spandanakoppaku <skoppaku@gmail.com>
2023-07-28 12:03:52 -0400

    Auto Push

--- vars.tf
+++ vars.tf
@@ -1,5 +1,5 @@
 variable "VPC_CIDR" {}
 # variable "var.ENV" {}
-variable "var.AZ" {}
-variable "PUBLIC_SUBNET_CIDR" {}
-variable "PRIVATE_SUBNET_CIDR" {}
+variable "var.AZ"{}
+variable "PUBLIC_SUBNET_CIDR"{}
+variable "PRIVATE_SUBNET_CIDR"{}
