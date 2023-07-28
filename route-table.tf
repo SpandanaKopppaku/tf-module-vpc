89de329f289a588d754db29c7ad1385a46ad57af
spandanakoppaku <skoppaku@gmail.com>
2023-07-28 11:49:00 -0400

    Auto Push

--- route-table.tf
+++ route-table.tf
@@ -25,10 +25,10 @@ resource "aws_route_table_association" "public_subnet_rt_association" {
 resource "aws_route_table" "private_rt" {
   vpc_id            = aws_vpc.main.id
 
-        # route {
-        #     cidr_block      = "10.0.0.0/0"
-        #     gateway_id      = aws_internet_gateway.igw.id
-        # }
+        route {
+            cidr_block      = "10.0.0.0/0"
+            gateway_id      = aws_internet_gateway.ngw.id
+        }
 
   tags = {
     Name = "roboshop-${var.ENV}-private-rt"
