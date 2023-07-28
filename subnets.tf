b771df413c94988c8e56f5d2163fc92d827356db
spandanakoppaku <skoppaku@gmail.com>
2023-07-27 20:24:08 -0400

    Auto Push

--- subnets.tf
+++ subnets.tf
@@ -1,21 +1,22 @@
 resource "aws_subnet" "public_subnet" {
-  count                  = lenght(var.PUBLIC_SUBNET_CIDR)
-  vpc_id                 = aws_vpc.main.id
-  cidr_block             = element(var.PUBLIC_SUBNET_CIDR, count.index)
-  availability_zone      = element(var.AZ, count.index)
+  count                     = lenght(var.PUBLIC_SUBNET_CIDR)
+  vpc_id                    = aws_vpc.main.id
+  cidr_block                = element(var.PUBLIC_SUBNET_CIDR, count.index)
+  availability_zone         = element(var.AZ, count.index)
+  map_public_ip_on_launch   = true
 
   tags = {
-    Name                 = "roboshop-${var.ENV}-public-subnet-${element(var.AZ, count.index)}"
+    Name                    = "roboshop-${var.ENV}-public-subnet-${element(var.AZ, count.index)}"
   }
 }
 
 resource "aws_subnet" "private_subnet" {
-  count                 = lenght(var.PRIVATE_SUBNET_CIDR)
-  vpc_id                = aws_vpc.main.id
-  cidr_block            = element(var.PRIVATE_SUBNET_CIDR, count.index)
-  availability_zone     = element(var.AZ, count.index)
+  count                    = lenght(var.PRIVATE_SUBNET_CIDR)
+  vpc_id                   = aws_vpc.main.id
+  cidr_block               = element(var.PRIVATE_SUBNET_CIDR, count.index)
+  availability_zone        = element(var.AZ, count.index)
 
   tags = {
-    Name                = "roboshop-${var.ENV}-private-subnet-${element(var.AZ, count.index)}"
+    Name                   = "roboshop-${var.ENV}-private-subnet-${element(var.AZ, count.index)}"
   }
 }
