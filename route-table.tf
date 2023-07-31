469ed3f36695ae68e2cd44eacd9eb3464bd83a84
spandanakoppaku <skoppaku@gmail.com>
2023-07-31 18:06:49 -0400

    Auto Push

--- route-table.tf
+++ route-table.tf
@@ -21,27 +21,6 @@ resource "aws_route_table_association" "public_subnet_rt_association" {
   route_table_id = aws_route_table.public_rt.id
 }
 
-# # Create Private Route Table
-# resource "aws_route_table" "private_rt" {
-#   vpc_id            = aws_vpc.main.id
-
-#         route {
-#             cidr_block          = "10.0.0.0/0"
-#             nat_gateway_id      = aws_nat_gateway.ngw.id
-#         }
-
-#   tags = {
-#     Name = "roboshop-${var.ENV}-private-rt"
-#   }
-# }
-
-# # Associates the route table to private subnet
-
-# resource "aws_route_table_association" "private_subnet_rt_association" {
-#   count          = length(aws_subnet.private_subnet.*.id)
-#   subnet_id      = element(aws_subnet.private_subnet.*.id, count.index)
-#   route_table_id = aws_route_table.private_rt.id
-# }
 
 # Creates Private Route Table
 resource "aws_route_table" "private_rt" {
